a8deaf27de01ba21a3dee08ba13fa76bb65bab42
DrosteEffect <DrosteEffect@users.noreply.github.com>
2020-08-22 06:45:41 +0200

    Update sRGB_to_XYZ.m

--- sRGB_to_XYZ.m
+++ sRGB_to_XYZ.m
@@ -1,17 +1,17 @@
-function xyz = srgb_to_xyz(rgb)
-% Convert a matrix of sRGB1 values to XYZ1.
+function xyz = sRGB_to_XYZ(rgb)
+% Convert an array of sRGB values to CIE 1931 XYZ values.
 %
-% (c) 2107-2019 Stephen Cobeldick
+% (c) 2107-2020 Stephen Cobeldick
 %
 %%% Syntax:
-% xyz = srgb_to_xyz(rgb)
+% xyz = sRGB_to_XYZ(rgb)
 %
 %% Example %%
 %
-% >> RGB = [64,128,255];
-% >> YXZ = srgb_to_xyz(RGB/255)*100
-% YXZ =
-%       27.88352542538389  23.74833281838537  97.72201291885087
+% >> rgb = [64,128,255]/255;
+% >> xyz = sRGB_to_XYZ(rgb)
+% xyz =
+%     0.2788    0.2375    0.9770
 %
 %% Input And Output Arguments %%
 %
@@ -20,7 +20,7 @@ function xyz = srgb_to_xyz(rgb)
 %       Size Nx3 or RxCx3, the last dimension encodes the R,G,B values.
 %
 %%% Output:
-% xyz = NumericArray, tristimulus values, scaled 1931 XYZ colorspace (Ymax==1).
+% xyz = NumericArray, tristimulus values, scaled XYZ colorspace (Ymax==1).
 %       The same size as <rgb>, the last dimension encodes the X,Y,Z values.
 %
 % See also XYZ_TO_SRGB XYZ_TO_CIECAM02
@@ -28,31 +28,41 @@ function xyz = srgb_to_xyz(rgb)
 %% Input Wrangling %%
 %
 isz = size(rgb);
-assert(isnumeric(rgb)&&isz(end)==3,'Input <rgb> must be an Nx3 or RxCx3 numeric array.')
-assert(isreal(rgb)&&all(0<=rgb(:)&rgb(:)<=1),'Input <rgb> values must be 0<=rgb<=1.')
+assert(isnumeric(rgb),'SC:sRGB_to_XYZ:NotNumeric',...
+	'1st input <rgb> must be a numeric array.')
+assert(isreal(rgb),'SC:sRGB_to_XYZ:Complex',...
+	'1st input <rgb> cannot be complex.')
+assert(isz(end)==3,'SC:sRGB_to_XYZ:InvalidSize',...
+	'1st input <rgb> last dimension must have size 3 (e.g. Nx3 or RxCx3).')
 rgb = reshape(rgb,[],3);
+assert(all(0<=rgb(:)&rgb(:)<=1),'',...
+	'1st input <rgb> values must be 0<=rgb<=1')
+%
+if ~isfloat(rgb)
+	rgb = double(rgb);
+end
 %
 %% RGB2XYZ %%
 %
-M = [...
-	+3.2406255,-1.5372080,-0.4986286;...
-	-0.9689307,+1.8757561,+0.0415175;...
-	+0.0557101,-0.2040211,+1.0569959];
-xyz = locGammaInv(rgb) / M.';
+M = [... High-precision sRGB to XYZ matrix:
+	0.4124564,0.3575761,0.1804375;...
+	0.2126729,0.7151522,0.0721750;...
+	0.0193339,0.1191920,0.9503041];
+xyz = sGammaInv(rgb) * M.';
 % Remember to include my license when copying my implementation.
 xyz = reshape(xyz,isz);
 %
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%srgb_to_xyz
-function rgb = locGammaInv(rgb)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%sRGB_to_xyz
+function rgb = sGammaInv(rgb)
 % Inverse gamma transform of sRGB data.
 idx = rgb <= 0.04045;
 rgb(idx) = rgb(idx) / 12.92;
 rgb(~idx) = real(((rgb(~idx) + 0.055) / 1.055).^2.4);
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%locGammaInv
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%sGammaInv
 %
-% Copyright (c) 2017 Stephen Cobeldick
+% Copyright (c) 2017-2020 Stephen Cobeldick
 %
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
