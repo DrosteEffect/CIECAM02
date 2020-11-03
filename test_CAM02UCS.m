7e811abf89cab2548463e0409a2e7ead162179ac
DrosteEffect <DrosteEffect@users.noreply.github.com>
2020-11-03 20:06:59 +0100

    Update test_CAM02UCS.m

--- test_CAM02UCS.m
+++ test_CAM02UCS.m
@@ -1,9 +1,9 @@
-function test_Jab()
-% Compare CIECAM02_TO_JAB and JAB_TO_CIECAM02 outputs against test values.
+function test_CAM02UCS()
+% Test CIECAM02_TO_CAM02UCS and CAM02UCS_TO_CIECAM02 against sample values.
 %
 % (c) 2017-2020 Stephen Cobeldick
 %
-% See also TEST_FUN CIECAM02_TO_JAB JAB_TO_CIECAM02
+% See also TEST_FUN CIECAM02_TO_CAM02UCS CAM02UCS_TO_CIECAM02
 
 % Initialize test function:
 test_fun(mfilename())
@@ -12,59 +12,59 @@ JMh = struct('J',50,'M',20,'h',10);
 Jab = [62.96296296,16.22742674,2.86133316];
 K_L=1.00; c1=0.007; c2=0.0228; % UCS
 %
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters(K_L,c1,c2))
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters('UCS'))
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters())
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters(K_L,c1,c2))
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters('UCS'))
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters())
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters(K_L,c1,c2))
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters('UCS'))
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters())
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters(K_L,c1,c2))
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters('UCS'))
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters())
 %
 JMh = struct('J',10,'M',60,'h',100);
 Jab = [15.88785047,-6.56546789,37.23461867];
 %
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters(K_L,c1,c2))
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters('UCS'))
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters())
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters(K_L,c1,c2))
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters('UCS'))
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters())
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters(K_L,c1,c2))
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters('UCS'))
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters())
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters(K_L,c1,c2))
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters('UCS'))
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters())
 %
 JMh = struct('J',50,'M',20,'h',10);
 Jab = [81.77008177,18.72061994,3.30095039];
 K_L=0.77; c1=0.007; c2=0.0053; % LCD
 %
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters(K_L,c1,c2), true)
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters('LCD'),     true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters(K_L,c1,c2), true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters('LCD'),     true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters('LCD'),     true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters('LCD'),     true)
 %
 JMh = struct('J',10,'M',60,'h',100);
 Jab = [20.63357204,-9.04659289,51.30577777];
 %
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters(K_L,c1,c2), true)
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters('LCD'),     true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters(K_L,c1,c2), true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters('LCD'),     true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters('LCD'),     true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters('LCD'),     true)
 %
 JMh = struct('J',50,'M',20,'h',10);
 Jab = [50.77658303,14.80756375,2.61097301];
 K_L=1.24; c1=0.007; c2=0.0363; % SCD
 %
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters(K_L,c1,c2), true)
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters('SCD'),     true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters(K_L,c1,c2), true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters('SCD'),     true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters('SCD'),     true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters('SCD'),     true)
 %
 JMh = struct('J',10,'M',60,'h',100);
 Jab = [12.81278263,-5.5311588,31.36876036];
 %
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters(K_L,c1,c2), true)
-test_fun(Jab, @CIECAM02_to_Jab, JMh, Jab_parameters('SCD'),     true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters(K_L,c1,c2), true)
-test_fun(JMh, @Jab_to_CIECAM02, Jab, Jab_parameters('SCD'),     true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(Jab, @CIECAM02_to_CAM02UCS, JMh, CAM02UCS_parameters('SCD'),     true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters(K_L,c1,c2), true)
+test_fun(JMh, @CAM02UCS_to_CIECAM02, Jab, CAM02UCS_parameters('SCD'),     true)
 %
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Jab_test
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%test_CAM02UCS
 %
 % Copyright (c) 2017-2020 Stephen Cobeldick
 %
