dede14dc59108fc758fb9ea6dc5e4ccbc435f6fa
DrosteEffect <DrosteEffect@users.noreply.github.com>
2025-12-05 13:26:01 +0100

    Accept integer sRGB
    * Improve documentation.
    * Accept integer sRGB arrays.
    * Replace Apache2.0 with MIT license.

--- CAM02UCS_to_CIECAM02.m
+++ CAM02UCS_to_CIECAM02.m
@@ -24,7 +24,8 @@ function out = CAM02UCS_to_CIECAM02(Jab,prm,isd)
 %   Jab = Double/single array, CAM02 perceptually uniform colorspace values J',a',b'.
 %         Size Nx3 or RxCx3, the last dimension encodes the J'a'b' values.
 %   prm = ScalarStructure of parameters from the function CAM02UCS_PARAMETERS.
-%   isd = ScalarLogical, true/false** = euclidean distance/reference J' values.
+%   isd = true    -> scaled J' for euclidean distance calculations (divided by K_L)
+%       = false** -> reference J' values (no scaling).
 %
 %% Output Arguments %%
 %
@@ -93,17 +94,11 @@ ang = mod(360*atan2(Y,X)/(2*pi),360);
 ang(Y==0 & X==0) = 0;
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%myAtan2d
-%
 % Copyright (c) 2017-2025 Stephen Cobeldick
 %
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
+% Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 %
-% http://www.apache.org/licenses/LICENSE-2.0
+% The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 %
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and limitations under the License.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%license
--- sRGB_to_CIEXYZ.m
+++ sRGB_to_CIEXYZ.m
@@ -5,46 +5,59 @@ function XYZ = sRGB_to_CIEXYZ(rgb)
 %
 %   XYZ = sRGB_to_CIEXYZ(rgb)
 %
-%% Example %%
+%% Examples %%
 %
 %   >> rgb = [64,128,255]/255;
 %   >> XYZ = sRGB_to_CIEXYZ(rgb)
 %   XYZ =
 %        0.27884    0.23748    0.97722
 %
+%   >> rgb = uint8([64,128,255]);
+%   >> XYZ = sRGB_to_CIEXYZ(rgb)
+%   XYZ =
+%        0.27884    0.23748    0.97722
+%
 %% Input Arguments %%
 %
-%   rgb = Double/single array of sRGB values to convert, scaled so that 0<=rgb<=1.
+%   rgb = Numeric array of sRGB values to convert. Floating point values
+%         must be 0<=rgb<=1, integer must be 0<=rgb<=intmax(class(rgb)).
 %         Size Nx3 or RxCx3, the last dimension encodes the R,G,B values.
 %
 %% Output Arguments %%
 %
-%   XYZ = Double/single array of converted tristimulus values. Values
-%         defined by the 1931 XYZ colorspace, scaled such that Ymax==1.
+%   XYZ = Double/single array of converted tristimulus values. Values are
+%         defined by the CIE 1931 XYZ colorspace, scaled such that Ymax==1.
 %         Size Nx3 or RxCx3, the last dimension encodes the X,Y,Z values.
 %
 %% Dependencies %%
 %
 % * MATLAB R2009a or later.
 %
-% See also CIEXYZ_TO_SRGB SRGB_TO_CAM02UCS CIEXYZ_TO_CIECAM02
+% See also CIEXYZ_TO_SRGB SRGB_TO_CAM02UCS SRGB_TO_JZAZBZ SRGB_TO_OKLAB
 
 %% Input Wrangling %%
 %
 isz = size(rgb);
-assert(isfloat(rgb),'SC:sRGB_to_CIEXYZ:rgb:NotFloat',...
-	'1st input <rgb> must be a floating point array.')
+if isfloat(rgb)
+	mxv = 1;
+elseif isinteger(rgb)
+	mxv = intmax(class(rgb));
+	rgb = double(rgb)./double(mxv);
+else
+	error('SC:sRGB_to_CIEXYZ:rgb:NotNumeric',...
+	'1st input <rgb> must be a numeric array, not %s',class(rgb))
+end
 assert(isreal(rgb),'SC:sRGB_to_CIEXYZ:rgb:NotReal',...
 	'1st input <rgb> must be a real array (not complex).')
 assert(isz(end)==3,'SC:sRGB_to_CIEXYZ:rgb:InvalidSize',...
 	'1st input <rgb> last dimension must have size 3 (e.g. Nx3 or RxCx3).')
 rgb = reshape(rgb,[],3);
 assert(all(0<=rgb(:)&rgb(:)<=1),'SC:sRGB_to_CIEXYZ:rgb:OutOfRange',...
-	'1st input <rgb> values must be 0<=rgb<=1')
+	'1st input <rgb> values must be 0<=rgb<=%d',mxv)
 %
 %% RGB2XYZ %%
 %
-M = [... IEC 61966-2-1:1999
+M = [... IEC 61966-2-1:1999, used for compatibility with other implementations.
 	0.4124,0.3576,0.1805;...
 	0.2126,0.7152,0.0722;...
 	0.0193,0.1192,0.9505];
